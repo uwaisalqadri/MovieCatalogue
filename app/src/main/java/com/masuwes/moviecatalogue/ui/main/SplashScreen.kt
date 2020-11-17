9bd56bcf82d3aa4ec363f1562aebddffb80d3e2d
uwaisalqadri <uwaisalqadri654321@gmail.com>
2020-11-17 16:01:32 +0700

    deprecated code fixed

--- app/src/main/java/com/masuwes/moviecatalogue/ui/main/SplashScreen.kt
+++ app/src/main/java/com/masuwes/moviecatalogue/ui/main/SplashScreen.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.masuwes.moviecatalogue.R
 
 class SplashScreen : AppCompatActivity() {
 
-    private val handler =  Handler()
+    private val handler =  Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
